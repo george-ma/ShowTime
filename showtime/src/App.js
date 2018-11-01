73af1f4559fef93f1fb03bcd8064d3f8ae4b56dd
george-ma <georgema86@gmail.com>
2018-10-31 21:11:18 -0400

    react-bootstrap

--- showtime/src/App.js
+++ showtime/src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Table from './Table';
+import NavBar from './NavBar';
+import { Button } from 'react-bootstrap';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>ShowTime</h1>
+                <NavBar/>
+                {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+                <Button bsStyle="primary">Primary</Button>
+                <Table/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ showtime/src/Table.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+
+/** Class Component
+ * A class component must include render(), 
+ * and the return can only return one parent element
+ */
+class Table extends Component {
+    render() {
+        return (
+            <table>
+                <TableHeader />
+                <TableBody />
+            </table>
+        );
+    }
+}
+
+// Simple Component
+const TableHeader = () => { 
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    );
+}
+
+// Simple Component
+const TableBody = () => { 
+    return (
+        <tbody>
+            <tr>
+                <td>Charlie</td>
+                <td>Janitor</td>
+            </tr>
+            <tr>
+                <td>Mac</td>
+                <td>Bouncer</td>
+            </tr>
+            <tr>
+                <td>Dee</td>
+                <td>Aspiring actress</td>
+            </tr>
+            <tr>
+                <td>Dennis</td>
+                <td>Bartender</td>
+            </tr>
+        </tbody>
+    );
+}
+
+export default Table;

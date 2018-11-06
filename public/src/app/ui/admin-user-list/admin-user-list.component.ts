119313d7ae1e738dec786d45a02b54759c06c23c
JustinLokHinWu <justin4827@gmail.com>
2018-11-06 02:42:50 -0500

    Linked admin user list to local storage

--- public/src/app/ui/admin-user-list/admin-user-list.component.ts
+++ public/src/app/ui/admin-user-list/admin-user-list.component.ts
@@ -11,113 +11,35 @@ export class AdminUserListComponent implements OnInit {
 
   constructor() { }
 
-  loadDummyUsers() {
-    this.users = [
-      {
-          "email": "george@gmail.com",
-          "username": "george",
-          "password": "hellogeorge",
-          "is_admin": false,
-          "is_banned": true 
-      },  
-      {
-          "email": "nick@gmail.com",
-          "username": "nick",
-          "password": "hellonick",
-          "is_admin": false,
-          "is_banned": false 
-      },  
-      {
-          "email": "bob@gmail.com",
-          "username": "bob",
-          "password": "hellobob",
-          "is_admin": true,
-          "is_banned": true 
-      },  
-      {
-          "email": "sam@gmail.com",
-          "username": "sam",
-          "password": "hellosam",
-          "is_admin": false,
-          "is_banned": false 
-      },  
-      {
-          "email": "tom@gmail.com",
-          "username": "tom",
-          "password": "hellotom",
-          "is_admin": false,
-          "is_banned": true 
-      },  
-      {
-          "email": "tim@gmail.com",
-          "username": "tim",
-          "password": "hellotim",
-          "is_admin": false,
-          "is_banned": true 
-      },  
-      {
-          "email": "harry@gmail.com",
-          "username": "harry",
-          "password": "helloharry",
-          "is_admin": false,
-          "is_banned": false 
-      },  
-      {
-          "email": "peter@gmail.com",
-          "username": "peter",
-          "password": "hellopeter",
-          "is_admin": false,
-          "is_banned": true 
-      },  
-      {
-          "email": "rick@gmail.com",
-          "username": "rick",
-          "password": "hellorick",
-          "is_admin": false,
-          "is_banned": true 
-      },  
-      {
-          "email": "jimmy@gmail.com",
-          "username": "jimmy",
-          "password": "hellojimmy",
-          "is_admin": false,
-          "is_banned": true 
-      },  
-      {
-          "email": "mike@gmail.com",
-          "username": "mike",
-          "password": "hellomike",
-          "is_admin": false,
-          "is_banned": true 
-      },  
-      {
-          "email": "jane@gmail.com",
-          "username": "jane",
-          "password": "hellojane",
-          "is_admin": true,
-          "is_banned": true 
-      }
-    ]
-  }
-
   ngOnInit() {
-    // dummy users
-    this.loadDummyUsers();
+    this.fetchUsers();
   }
 
   promoteUser(user) {
-    // TODO
-    console.log(`TODO: promote ${user.username} to admin`)
+    console.log(`Promoting ${user.username} to admin`);
+    user.is_admin = true;
+    this.updateUsers();
   }
 
   banUser(user) {
-    // TODO
-    console.log(`TODO: ban ${user.username}`)
+    console.log(`Banning ${user.username}`);
+    user.is_banned = true;
+    this.updateUsers();
   }
 
   unbanUser(user) {
-    // TODO
-    console.log(`TODO: unban ${user.username}`)
+    console.log(`Unbanning ${user.username}`);
+    user.is_banned = false;
+    this.updateUsers();
+
+  }
+
+  fetchUsers() {
+    this.users =  JSON.parse(sessionStorage.getItem('users'));
+  }
+
+  updateUsers() {
+    sessionStorage.setItem('users', JSON.stringify(this.users));
   }
 
 }

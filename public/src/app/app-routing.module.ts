760335c496e6e33db0e7abc6c0dc491b1d8d055e
JustinLokHinWu <justin4827@gmail.com>
2018-11-06 00:45:55 -0500

    Added the UI for the admin's user list

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './ui/login/login.component';
 import { GridComponent } from './ui/grid/grid.component';
 import { RegisterComponent } from './ui/register/register.component';
+import { AdminUserListComponent } from './ui/admin-user-list/admin-user-list.component'
 
 const routes: Routes = [
   { path: '', component: GridComponent},
   { path: 'grid', component: GridComponent},
   { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent}
+  { path: 'register', component: RegisterComponent},
+  { path: 'adminuserlist', component: AdminUserListComponent},
 ];
 
 @NgModule({
--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UiModule } from './ui/ui.module';
+import { AdminUserListComponent } from './ui/admin-user-list/admin-user-list.component';
 
 @NgModule({
   declarations: [ // Component, Directive, or Pipe
-    AppComponent
+    AppComponent, AdminUserListComponent
   ],
   imports: [ 
     BrowserModule,

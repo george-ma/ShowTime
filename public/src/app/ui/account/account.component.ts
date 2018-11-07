da246dca9fc4e343e5fdb3aeb29a11260f8275e1
Karl-Cui <karlcui174@gmail.com>
2018-11-07 11:08:51 -0800

    account setting changes updates users list

--- public/src/app/ui/account/account.component.ts
+++ public/src/app/ui/account/account.component.ts
@@ -23,8 +23,30 @@ export class AccountComponent implements OnInit {
   }
 
   updateUserInfo() {
+    // update user info in user list
+    let users = JSON.parse(sessionStorage.getItem('users'));
+    for (let curUser of users) {
+      if (curUser.username == this.user.username) {
+        if (this.updateUser.email) {
+          curUser.email = this.updateUser.email;
+        }
+        if (this.updateUser.username) {
+          curUser.username = this.updateUser.username;
+        }
+        if (this.updateUser.password) {
+          curUser.password = this.updateUser.password;
+        }
+        break;
+      }
+    }
+    sessionStorage.setItem('users', JSON.stringify(users));
+
+    // update currentUser
     if (this.updateUser.profilePic) {
       this.user.profilePic = this.updateUser.profilePic;
+    }
+    if (this.updateUser.email) {
+      this.user.email = this.updateUser.email;
     }
     if (this.updateUser.username) {
       this.user.username = this.updateUser.username;

d0cbbc49ecfe3daaf787da29e27e9e18047d3e63
Sohail Hameed <3370sohail@gmail.com>
2018-11-07 19:02:32 -0500

    local review changes (#41)

--- public/src/app/ui/show/show.component.ts
+++ public/src/app/ui/show/show.component.ts
@@ -12,10 +12,15 @@ import { Show } from '../models/show';
 export class ShowComponent implements OnInit {
 
   show: any = {};
+  user: any = {};
+  myShow: any = {};
+
+
   constructor( private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.getShow(this.route.snapshot.paramMap.get('id'));
+    this.getUser()
   }
 
   getShow(id){
@@ -25,7 +30,34 @@ export class ShowComponent implements OnInit {
         this.show = show;
       }
     }
-    console.log(this.show , id)
+  }
+
+  getCheckUser() {
+    return sessionStorage.getItem('currentUser') != null;
+  }
+  getUser(){
+    if(sessionStorage.getItem('currentUser') != null){
+      this.user = JSON.parse(sessionStorage.getItem('currentUser'));
+      this.setMyShow();
+      return true;
+    }
+    return false;
+  }
+
+  setMyShow(){
+    for(let my_show of this.user.my_shows){
+      if(my_show.id == this.show.id){
+        this.myShow = my_show;
+      }
+    }
+  }
+
+  setRating(stars){
+    this.myShow.rating = stars;
+  }
+
+  setStatus(status){
+    this.myShow.status = status;
   }
 
 

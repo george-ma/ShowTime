04fb80b58da3b486ffca23673aeb1f358fad0efe
Karl-Cui <karlcui174@gmail.com>
2018-11-07 20:32:47 -0800

    Change countdown timer on shows in edit

--- public/src/app/ui/edit-show/edit-show.component.ts
+++ public/src/app/ui/edit-show/edit-show.component.ts
@@ -16,9 +16,37 @@ export class EditShowComponent implements OnInit {
   shows: Array<Show> = []
   updateShow = new Show(-1, '', '', false, 'assets/noImage.jpg', '')
 
+  months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  hours = [];
+  days = [];
+  years = [];
+  date = {};
+
+  airingChecked = false;
+  intervalChecked = false;
+
   constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+
+    for(let i  = 0; i < 24; i++) {
+      this.hours[i] = i;
+    }
+    for(let i = 1; i <= 31; i++) {
+      this.days[i] = i;
+    }
+    for(let i = 0; i < 30; i++) {
+      this.years[i] = (currentYear - 15) + i;
+    }
+
+    // TODO: hour doesn't fill properly
+    this.date["hour"] = this.hours[currentDate.getHours()];
+    this.date["day"] = currentDate.getDate();
+    this.date["month"] = this.months[currentDate.getMonth()];
+    this.date["year"] = currentYear;
+
     this.shows = JSON.parse(sessionStorage.getItem('shows'));
     this.user = JSON.parse(sessionStorage.getItem('currentUser'));
     this.show = this.getShow(this.route.snapshot.paramMap.get('id'));
@@ -39,11 +67,35 @@ export class EditShowComponent implements OnInit {
     }
   }
 
+  setAirDate() {
+    if (this.airingChecked) {
+      let airDate = new Date();
+
+      airDate.setFullYear(this.date["year"]);
+      airDate.setMonth(this.months.indexOf(this.date["month"]));
+      airDate.setDate(this.date["day"]);
+      airDate.setHours(this.date["hour"]);
+      airDate.setMinutes(0);
+      airDate.setSeconds(0);
+      airDate.setMilliseconds(0);
+
+      // NaN if date is invalid, which would fail this
+      if (airDate.getTime() === airDate.getTime()) {
+        this.updateShow.airDate = airDate;
+
+      } else {
+        this.updateShow.airDate = undefined;
+      }
+    }
+  }
+
   getCheckAdmin() {
     return this.user.is_admin;
   }
 
   submitEdits() {
+    this.setAirDate();
+
     if (this.getCheckAdmin()) {
       this.updateShow.approved = true;
       for (let i = 0; i < this.shows.length; i++) {

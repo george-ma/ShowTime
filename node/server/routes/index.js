1092c08e04eee4664cd9408cebcc14b4a669498d
Karl-Cui <karlcui174@gmail.com>
2018-11-24 22:21:44 -0500

    Endpoints for shows and user

--- node/server/routes/index.js
+++ node/server/routes/index.js
@@ -22,16 +22,24 @@ module.exports = (app) => {
     app.post('/users/:id/update', usersController.update);
     // remove user by id
     app.get('/users/:id/delete', usersController.remove);
-    //login a user
+    // login a user
     app.post('/users/login', usersController.loginUser);
+    // add show to user's list of shows
+    app.post('/users/:id/addshow', usersController.addShow);
+    // removes show from user's list of shows
+    app.post('/users/:id/removeshow', usersController.removeShow);
+    // get user's shows
+    app.get('/users/:id/myshows', usersController.getMyShows);
+    // get not user's shows
+    app.get('/users/:id/notmyshows', usersController.getNotMyShows);
 
     // * show routes *
     // create a new show
     app.post('/shows', showsController.create);
     // get all shows
-    app.get('/shows', showsController.getAllShows);
-
-
+    app.get('/shows/approved', showsController.getApprovedShows);
+    // get unapproved shows
+    app.get('/shows/unapproved', showsController.getUnapprovedShows);
 
 
 

743e4a9d0bc6cc17903dbf373803cefdca1ab6a4
Karl-Cui <karlcui174@gmail.com>
2018-11-24 22:44:00 -0500

    Get shows via backend

--- public/src/app/ui/grid/grid.service.ts
+++ public/src/app/ui/grid/grid.service.ts
@@ -1,18 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from '../../http.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GridService {
 
-  constructor() { }
+  constructor(private httpClient: HttpService) { }
 
-  addShow(shows) {
-    sessionStorage.setItem('shows', JSON.stringify(shows));
+  API_URL = 'http://localhost:8000';
+
+  // addShow(shows) {
+  //   sessionStorage.setItem('shows', JSON.stringify(shows));
+  // }
+  //
+  // getShows() {
+  //   return JSON.parse(sessionStorage.getItem('shows'))
+  // }
+
+  addShow(data) {
+    return this.httpClient.post(`${this.API_URL}/shows`, data);
   }
 
-  getShows() {
-    return JSON.parse(sessionStorage.getItem('shows'))
+  getApprovedShows() {
+    return this.httpClient.get(`${this.API_URL}/shows/approved`);
   }
 
+  getUnapprovedShows() {
+    return this.httpClient.get(`${this.API_URL}/shows/unapproved`);
+  }
+
+  getMyShows(param) {
+    return this.httpClient.get(`${this.API_URL}/users/${param}/myshows`);
+  }
+
+  getNotMyShows(param) {
+    return this.httpClient.get(`${this.API_URL}/users/${param}/notmyshows`);
+  }
+
+  getAllUsers() {
+    return this.httpClient.get(`${this.API_URL}/users`);
+  }
 }
